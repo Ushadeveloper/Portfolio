3a565d837509df355a0bac48fda16076017ee622
U_s H_a <ushausama2@outlook.com>
2024-01-11 20:13:13 +0500

    map and filter guest data

--- src/components/table.componets.jsx
+++ src/components/table.componets.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import '../table.css';
 
 
+
 function ShowTableData() {
 
     const guestData = [
@@ -13,59 +14,140 @@ function ShowTableData() {
 
     ];
 
-    const [gstbl, setGstbl] = useState(guestData);
-    // console.log("table "+gstbl);
-    const [filtergs, setFiltergs] = useState();
-    // console.log("filter "+filtergs);
+    //////////////////create state tblGuest to save data in state///////////////////////////
 
+    const [tblGuest, setTblGuest] = useState(guestData);
+    // console.log('table of Guest',tblGuest);
+
+    //////////////////create state filterGuestTbl to filter data in state///////////////////////////
+
+    const [filterGuestTbl, setFilterGuestTbl] = useState('');
+    // console.log('filter of Guest', filterGuestTbl);
+
+
+    //////////////////create handleFilterChange event to trigger the input  ///////////////////////////
 
     const handleFilterChange = (e) => {
-        
-        setFiltergs(e.target.value)
-    }
 
-    const handleTableData = () => {
-        setGstbl()
+        setFilterGuestTbl(e.target.value)
     }
-    console.log(filtergs)
-    const filteredTable = gstbl.filter(guest =>
- guest.Name.toLowerCase().includes((filtergs))
+    //////////////////create filteredGuestTable  to filter the input  ///////////////////////////
+
+    const filteredGuestTable = tblGuest.filter(guest =>
+        guest.Name.toLowerCase().includes((filterGuestTbl).toLowerCase())
         // guest.Id.includes(filtergs)==='usama'
         // guest.Name.toLowerCase().includes(filtergs.toLowerCase()) ||
         // guest.Phone.includes(filtergs) ||
         // guest.Address.toLowerCase().includes(filtergs.toLowerCase())
-      );
+    );
+
+    // const [saveData, setSaveData] = useState(guestData);
+    // console.log('savedata =', saveData);
+
+    // const handleSaveChange = (e) => {
+    //     setSaveData(e.target.value)
+    // }
+
+
+    // const handleSaveSubmit = (e) => {
+    //     e.preventDefault();
+    //     const newEntries = {Id: saveData.Id, Name: saveData.Name, Phone: saveData.Phone, Address: saveData.Address}
+    //     setSaveData([...saveData, newEntries])
+
+    // }
+    // console.log('setsavedata  = ',setSaveData)
+    //     evnt.preventDefault();
+    // console.log("table "+gstbl);
+    // const [filtergs, setFiltergs] = useState("");
+    // console.log("filter "+filtergs);
+    // const [formInputData, setformInputData] = useState(
+    //     {
+    //         Id: '',
+    //         Name: '',
+    //         Phone: '',
+    //         Address: ''
+    //     }
+    // );
+    // const handleChange = (evnt) => {
+    //     const newInput = (data) => ({
+    //           [evnt.target.name]: evnt.target.value 
+
+
+
+    //         })
+
+    //     setformInputData(newInput)
+    // }
+
+
+    // const handleSubmit = (evnt) => {
+    //     evnt.preventDefault();
+    //     const checkEmptyInput = !Object.values(formInputData).every(res => res === "")
+    //     if (checkEmptyInput) {
+    //         const newData = (data) => ([...data, formInputData])
+    //         setTableData(newData);
+    //         const emptyInput = { 
+    //              Id: '',
+    //         Name: '',
+    //         Phone: '',
+    //         Address: '' }
+    //         setformInputData(emptyInput)
+    //     }
+    // }
+
+    // const handleGsChange = (e) => {
+    //     setGstbl(e.target.value)
+
+    //     Name = ""
+
+    // }
+
 
 
     return (
 
         <>
             <h1>Guest Data</h1>
+            <div className="form-container">
+                {/* <form className="user-form">
+                    <label htmlFor="id">ID:</label>
+                    <input type="text" id="id" name="Id" value={saveData.Id} onChange={handleSaveChange} />
 
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" id="name" name="Name" value={saveData.Name} onChange={handleSaveChange} />
+
+                    <label htmlFor="phone">Phone:</label>
+                    <input type="tel" id="phone" name="Phone" value={saveData.Phone} onChange={handleSaveChange} />
+
+                    <label htmlFor="address">Address:</label>
+                    <textarea id="address" name="Address" rows="2" value={saveData.Address} onChange={handleSaveChange}></textarea>
+
+                    <button value={handleSaveSubmit} type="submit">Submit</button>
+                </form> */}
+            </div>
             {/* <div className="form-container">
-                <label for="name"><b>ID</b></label>
-                <input type="text" placeholder="Enter Guest ID" name="id" required />
-                <label for="name"><b>Name</b></label>
-                <input type="text" placeholder="Enter Guest Name" name="name" required />
+                <label ><b>ID</b></label>
+                <input type="text" placeholder="Enter Guest ID" name="Id" onChange={handleChange} value={formInputData.Id} />
+                <label ><b>Name</b></label>
+                <input type="text" placeholder="Enter Guest Name" name="Name" onChange={handleChange} value={formInputData.Name} />
 
-                <label for="phone"><b>Phone</b></label>
-                <input type="number" placeholder="Enter Guest Phone" name="Phone" required />
+                <label ><b>Phone</b></label>
+                <input type="number" placeholder="Enter Guest Phone" name="Phone" onChange={handleChange} value={formInputData.Phone} />
 
-                <label for="address"><b>Address</b></label>
-                <input type="text" placeholder="Enter Guest Address"  name="Address" required />
+                <label ><b>Address</b></label>
+                <input type="text" placeholder="Enter Guest Address" name="Address" onChange={handleChange} value={formInputData.Address} />
 
-                 <label for="brand"><b>Brand</b></label>
-                    <input type="text" placeholder="Enter Guest Brand" name="brand" required /> 
+                
 
-                <input type="submit" value="Submit" />
+                <input type="submit" onClick={handleSubmit} value="Submit" />
             </div> */}
 
 
             <div className="table-container">
 
-                
-                <input type="text" placeholder="Search by Id, Name, Phone, or Address" value={filtergs} onChange={handleFilterChange} />
-              
+
+                <input type="text" placeholder="Search by Id, Name, Phone, or Address" value={filterGuestTbl} onChange={handleFilterChange} />
+
                 <table className="product-table">
                     <thead className='thread'>
                         <tr>
@@ -76,13 +158,32 @@ function ShowTableData() {
                         </tr>
                     </thead>
                     <tbody className='tbody'>
-                        {filteredTable.map(guest =>
-                            <tr><td>{guest.Id}</td>
+                        {filteredGuestTable.map(guest =>
+                            <tr key={guest.Id}><td>{guest.Id}</td>
                                 <td>{guest.Name}</td>
                                 <td>{guest.Phone}</td>
                                 <td>{guest.Address}</td></tr>)}
                     </tbody>
                 </table>
+
+
+                {/* <table className="product-table">
+                    <thead className='thread'>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Phone</th>
+                            <th>Address</th>
+                        </tr>
+                    </thead>
+                    <tbody className='tbody'>
+                        {gstbl.map(guest =>
+                            <tr><td>{guest.Id}</td>
+                                <td>{guest.Name}</td>
+                                <td>{guest.Phone}</td>
+                                <td>{guest.Address}</td></tr>)}
+                    </tbody>
+                </table> */}
 
             </div>
         </>
